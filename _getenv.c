01d7dc55a9128af773272f438d9c39537d9cc4fe
Romina Guimaraens <5071@holbertonschool.com>
2022-08-01 15:23:17 -0700

    shell

---
+++ _getenv.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char *_getenv(const char *name, char **environ)
+{
+	int i = 0;
+	char *duplicate, *token;
+
+	for (; environ; i++)
+	{
+		duplicate = _strdup(environ[i]);
+
+		token = strtok(duplicate, "=");
+
+		if (strcmp(duplicate, name) == 0)
+		{
+			token = strtok(NULL, "=");
+			return (token);
+		}
+		else
+			continue;
+	}
+	return (0);
+}
